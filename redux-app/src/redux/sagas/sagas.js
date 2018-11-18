91d5ed2740f64c41a044d90cd2dadcfae873b46a
Jay Moore <jmoore@ehealthinnovation.org>
2018-11-18 13:40:08 -0500

    cache sagas to prevent duplicate network requests

--- redux-app/src/redux/sagas/sagas.js
+++ redux-app/src/redux/sagas/sagas.js
@@ -6,16 +6,11 @@ import {
     LOAD_VALUESET
 } from '../../redux/actions/actions'
 
-import { put, takeLatest } from 'redux-saga/effects'
-
+import { put, take, fork } from 'redux-saga/effects'
 
 
 function* fetchValueSet(action) {
 
-    // ???: takeLatest means that when this function is called 3x concurrently,
-    // ???: it will start a function, then cancel that function and start a new one, then cancel
-    // ???: that function and start a new one.
-    console.log('XXX')
     yield put(setValueSetIsLoading(action.valueSetId, true))
 
     const url = 'http://hapi.fhir.org/baseDstu3/ValueSet/' + action.valueSetId
@@ -34,15 +29,20 @@ function* fetchValueSet(action) {
         yield put(setValueSetError(action.valueSetId, error))
     }
 
-    console.log('YYY')
     yield put(setValueSetIsLoading(action.valueSetId, false))
 
 }
 
 
 export default function* watchFetchValueSet() {
-    // !!!: having to deal with the edge case here feels strange, and none of the options are what i want:
-    // !!!: - take the first request and let it finish, hookup all the subsequent requests to also get notified
-    // !!!: - this is also a hint that the famed redux devtool is kinda noisy
-    yield takeLatest(LOAD_VALUESET, fetchValueSet)
+    // !!!: to prevent duplicate network requests, we need to cache actions
+    const tasks = {} // { valueSetId: task }
+
+    while (true) {
+        const action = yield take(LOAD_VALUESET)
+        const previousTask = tasks[action.valueSetId]
+        if (previousTask === undefined || previousTask.isRunning() === false) {
+            tasks[action.valueSetId] = yield fork(fetchValueSet, action)
+        }
+    }
 }
