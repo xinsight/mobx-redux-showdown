5eb8d066b48b6de4362674e646ccaf2c9398c226
Mark Erikson <mark@isquaredsoftware.com>
2018-11-25 17:40:02 -0500

    Add data fetching thunk

--- redux-app/src/redux/actions/actions.js
+++ redux-app/src/redux/actions/actions.js
@@ -19,3 +19,28 @@ export const setValueSetIsLoading = (valueSetId, isLoading) => {
 export const setValueSetError = (valueSetId, error) => {
     return { type: SET_VALUESET_ERROR, valueSetId, error }
 }
+
+export function fetchValueSet(valueSetId) {
+    return async (dispatch, getState) => {
+        dispatch(setValueSetIsLoading(valueSetId, true));
+
+        const url = 'http://hapi.fhir.org/baseDstu3/ValueSet/' + valueSetId
+
+        try {
+            const response = await fetch(url);
+
+            const json = await response.json()
+
+            let items = json.compose.include[0].concept // FHIR fun
+            let valueSet = {} // {code: display}
+            items.forEach(item => valueSet[item.code] = item.display)
+
+            dispatch(setValueSet(valueSetId, valueSet))
+        } catch(error) {
+            dispatch(setValueSetError(valueSetId, error.message))
+        }
+
+        dispatch(setValueSetIsLoading(valueSetId, false))
+    }
+}
+
