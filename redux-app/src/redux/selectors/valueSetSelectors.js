5bf3b0459982ab8ec3ad5d3dd10d170fb87ca5eb
Mark Erikson <mark@isquaredsoftware.com>
2018-11-25 18:45:45 -0500

    Delete now-unused original logic

--- redux-app/src/redux/selectors/valueSetSelectors.js
+++
@@ -1,7 +0,0 @@
-import {createSelector} from "redux-starter-kit";
-
-
-export const selectValueSetById = createSelector(
-    ["valueSets", (state, props) => props.valueSet],
-    (valueSets, valueSetId) => valueSets[valueSetId]
-);
